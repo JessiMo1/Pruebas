2790eabc7162d825de146c6cd5886ebce7f24fad
JessiMo1 <jessimo22311@gmail.com>
2024-12-13 19:45:27 -0600

    Ejercicio de busqueda (prueba 2)

--- Letras.java
+++ Letras.java
@@ -1,29 +1,37 @@
 import java.util.*;
 public class Letras {
-    public static int busqueda(String s){
-        Set<Character> set  = new  HashSet<>();
-        int maxl = 0;
+    public static void busqueda(String s){
+        Set<Character> set  = new  HashSet<>(); //Ayudara a buscar los caracteres
+        int maxl = 0; //el tamaño de la longitud 
         int inicio = 0;
+        String longitudPalabra= ""; //Almacenará el palabra mas larga
+
          for (int fin = 0; fin < s.length(); fin++){
             char  caracterFin = s.charAt(fin);
 
-            while (set.contains(caracterFin)){
+            while (set.contains(caracterFin)){//si la letra ya se encuntra en conjunto esto ayudara al moverse al inicio
                 set.remove(s.charAt(inicio));
                 inicio++;
             }
+            //Agregara el carácter que tenemos actual
             set.add(caracterFin);
-            maxl = Math. max(maxl, fin - inicio + 1);
+            //Ayudará a calcular la longitud del substring
+            int length = fin - inicio + 1;
+            if (length > maxl) {
+                maxl = length;
+                longitudPalabra = s.substring(inicio, fin + 1); 
+            }
 
 
          }
-         return maxl;
+         System.out.println("El resultado es: \"" + longitudPalabra + "\", con una longitud de: " + maxl );
 
 
     }
     public static void main(String[] args) {
-        System.out.println(busqueda("abcabcbb"));
-        System.out.println(busqueda("bbbbb"));
-        System.out.println(busqueda("pwwkew"));
+        busqueda("abcabcbb");
+        busqueda("bbbbb");
+       busqueda("pwwkew");
     }
     
 }
